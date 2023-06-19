a05856548badd0afc4b4046fe56a4f8f7e5873ea
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-06-19 22:17:25 +0100

    feat: build responsive navbar component

---
+++ src/components/nav.tsx
@@ -0,0 +1,25 @@
+import './_nav.scss'
+import LOGO from '../../public/assets/Layer 2logo.png'
+import { BsChevronDown } from 'react-icons/bs'
+import { BiMenuAltRight } from 'react-icons/bi'
+
+const Nav = () => {
+  return (
+    <section>
+      <div className='logobox'><img src={LOGO} alt="inletsky" /></div>
+      <nav>
+        <span tabIndex={0}>SOLUTIONS <BsChevronDown fontSize={15} fontWeight={500} /></span>
+        <span tabIndex={0}>DEVELOPERS <BsChevronDown fontSize={15} fontWeight={500} /></span>
+        <span tabIndex={0}>CUSTOMERS</span>
+        <span tabIndex={0}>BLOG</span>
+      </nav>
+      <div className='ctabox'>
+        <a href="#">Login</a>
+        <a href="#">Signup</a>
+      </div>
+      <BiMenuAltRight className='hamburger' fontSize={36} />
+    </section>
+  )
+}
+
+export default Nav
