0cd02ebf5671b6e0edf1c1cd710dff094e182d54
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-06-20 10:40:21 +0100

    feat: build Hero component

---
+++ src/components/hero.tsx
@@ -0,0 +1,33 @@
+import './_hero.scss'
+import Nav from './nav'
+import BG from '../pages/home/assets/rm3j73batch2-16-011.png'
+import GLOBE from '../pages/home/assets/group284.png'
+import { HiArrowLongLeft } from 'react-icons/hi2'
+import { BsArrowRight } from 'react-icons/bs'
+
+const Hero = () => {
+  const style = {
+    backgroundImage: `url(${BG}), url(${GLOBE})`,
+    backgroundPosition: 'center, 50% 210px',
+    backgroundSize: 'cover, 1000px',
+    backgroundRepeat: 'repeat, no-repeat'
+  }
+  return (
+    <section className='hero' style={style}>
+      <Nav />
+      <div className='hero-typo'>
+        <a href="#">Explore what's new with inletsky <BsArrowRight /></a>
+        <h2>BrainNet. GL JS</h2>
+        <h5>BrainNet. GL JS is a Networking library for vector networking on  the Web. Its performance, real-time styling, and interactivity features set the bar for anyone building fast, immersive tech on the web.</h5>
+      </div>
+      <div className='hero-scrolldown'>
+        <HiArrowLongLeft />
+        <span>Explore Now</span>
+      </div>
+      <div className='blur'></div>
+      <div className='blurbg'></div>
+    </section>
+  )
+}
+
+export default Hero
