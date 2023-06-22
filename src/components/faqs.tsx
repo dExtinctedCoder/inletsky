5a40d263dabb00703f8f3d458c595861e99e2d32
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-06-22 06:57:01 +0100

    feat: build faqs section on homepage

---
+++ src/components/faqs.tsx
@@ -0,0 +1,89 @@
+import './_faqs.scss'
+
+// faqs section component
+const Faqs = () => {
+  return (
+    <section className='faqs'>
+      <li><span></span>Question people often asks</li>
+      <h2>FAQS</h2>
+      <ul>
+        <li>
+          <h4>What is Inletsky GL JS? <span tabIndex={1}>+</span></h4>
+          <p>With Atlas, you can self-host Inletsky maps and geocoding APIs, Streets, Satellite, and Terrain tilesets, and Inletsky Studio on your network, behind a firewall, or even air-gapped. Use Atlas to power on-premises applications using Inletsky GL JS v2 and Inletsky Maps SDKs for iOS and Android.</p>
+        </li>
+        <li>
+          <h4>What are the use cases for Inletsky GL JS?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>What browsers are supported?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>How do I get started using Inletsky GL JS?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>What are the new features in Inletsky GL JS v2?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>What are the performance improvements?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>What can I do with 3D rendering in Inletsky GL JS v2?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>What are the new features for 3D rendering?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>What is the free form camera API?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>What can I do with the free form camera?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>Which map projections are supported?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>What pricing is available with GL JS?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>Do I pay by the number of map tiles loaded?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>Offline Caching<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+        <li>
+          <h4>What are some upcoming features of Inletsky GL JS?<span tabIndex={1}>+</span></h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, neque numquam? Itaque aliquam cum exercitationem asperiores quod repellat dolores doloribus, laborum, praesentium est, atque sit amet. Quo vel assumenda veniam.
+            Similique soluta, ipsa placeat est accusamus nihil repellat temporibus tenetur. Placeat libero dolorem culpa cupiditate incidunt ab itaque. Quaerat quas nihil corporis incidunt consectetur, reiciendis id accusantium suscipit unde maiores!</p>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default Faqs
